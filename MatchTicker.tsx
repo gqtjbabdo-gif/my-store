3dc8a26730f256cf38b79072469890ef4de0fefc
gqtjbabdo-gif <gqtjbabdo@gmail.com>
2026-06-08 17:17:33 +0100

    Create MatchTicker.tsx

---
+++ MatchTicker.tsx
@@ -0,0 +1,12 @@
+import MatchTicker from './MatchTicker';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>أهلاً بك في Abdo Store</h1>
+      <MatchTicker />
+    </div>
+  );
+}
+
+export default App;
